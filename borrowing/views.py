f70d801309ba9fbfd036d1c8718aa93b936ded62
AnDy <ansmbox@gmail.com>
2023-03-26 20:58:43 +0300

    Implement business logic of borrowing book

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,10 @@
+from django.db import models, transaction
 from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from book.models import Book
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
@@ -35,3 +37,11 @@ class BorrowingViewSet(
             return BorrowingCreateSerializer
 
         return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        book_id = int(self.request.data.get("book"))
+        with transaction.atomic():
+            Book.objects.filter(id=book_id).update(
+                inventory=models.F("inventory") - 1
+            )
+            serializer.save(user=self.request.user)
